1b0aa7ba3a11b33a70f27e1a93e04f3220ad1781
infinumtech <ashahin@socialgist.com>
2022-07-22 01:31:17 +0300

    Adding Ability to handle Channels and videos
    The bot can now handle channels and videos all in one txt file

--- linkToDatabse.py
+++ linkToDatabse.py
@@ -16,6 +16,16 @@ API_ENDPOINT = "http://"+cusName+":"+authKey+"@localhost:8080/"+cusName+"/youtub
 for x in range(len(links)):
     time.sleep(1)
     links[x] =links[x].strip()
+    tempLink = str(links[x])
+    if ("watch" in links[x]):
+        print("Video Number "+x+ " Submitted")
+        API_ENDPOINT = "http://"+cusName+":"+authKey + "@localhost:8080/"+cusName+"/youtube/main/videos"
+    if("channel" in links[x]):
+        print("Channel Number "+x+" Submitted")
+        API_ENDPOINT = "http://"+cusName+":"+authKey + "@localhost:8080/"+cusName+"/youtube/main/channels"
+
+
+    
     stringToExport = (
         '\n{\n\t\"addAll\": {\n\n\t\t"url": "'+links[x]+'"\n\t}\n}')
     #print (stringToExport)
