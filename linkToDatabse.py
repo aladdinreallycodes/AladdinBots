4fd536038d8965590e8a8d88a4f727440c73c068
infinumtech <ashahin@socialgist.com>
2022-07-28 11:12:14 -0400

    Update linkToDatabse.py

--- linkToDatabse.py
+++ linkToDatabse.py
@@ -19,11 +19,12 @@ links = f.readlines()
 cusName = 'crimson'
 authKey = 'Rhbvcjy1122'
 
+failedLinks = []
 
 #API_ENDPOINT = "http://"+cusName+":"+authKey+"@localhost:8080/"+cusName+"/youtube/main/channels"
 
 bar = Bar('Processing', max=len(links))
-print("Adding "+str(len(links))+" To "+cusName)
+print("Adding "+str(len(links))+" Record(s) To "+cusName)
 
 for x in range(len(links)):
     #time.sleep(3)
@@ -50,7 +51,10 @@ for x in range(len(links)):
     bar.next()
     #print(r.text)
     if ("failure" in r.text):
-        print("Faild at Link:\n"+str(links[x]))
+        #print("Faild at Link:\n"+str(links[x]))
+        failedLinks.append(str(links[x]))
+
+print(failedLinks)
 bar.finish()
 
 
